6a32a8d5bf58a961abb8737598d688075d12dfec
Marc TAJCHMAN <marc.tajchman@cea.fr>
2024-07-16 16:01:59 +0200

    preconfiguration step in Refrop is only applied to source files newer than the corresponding compiled files

--- Modules/EOS/Src/EOS_Refprop10/REFPROP10_pre_configure.sh
+++ Modules/EOS/Src/EOS_Refprop10/REFPROP10_pre_configure.sh
@@ -26,7 +26,7 @@ if [ -f $EOS_BINARY_DIR/Tests/C++/Refprop10WaterVapor_pt.val ]; then exit 0; fi
 
 # ----------------------------------------
 # Sanity Check
-if [ ! -d $REFPROP10_ROOT_DIR && ! -f $REFPROP10_ROOT_DIR ] ; then
+if [[ ! -d $REFPROP10_ROOT_DIR && ! -f $REFPROP10_ROOT_DIR ]] ; then
     error 44 " T.M. Plugin EXT. not found --with-refprop10=$REFPROP10_ROOT_DIR"
 fi
 
@@ -78,9 +78,15 @@ fi
 # Source files
 echo "Copy source files"
 mkdir -p $BINARY_DIR/Refprop10
-cp $PLUGINEXT_SRC/* $BINARY_DIR/Refprop10/.
-# Patches
-(cd $BINARY_DIR/Refprop10 ; ./REFPROP10_patch.sh)
+for fSrc in $PLUGINEXT_SRC/*
+do
+  fDest="$BINARY_DIR/Refprop10/"`basename $fSrc`
+  if test ! -f $fDest -o $fSrc -nt $fDest
+  then
+    cp $fSrc $fDest
+   (cd $BINARY_DIR/Refprop10 ; ./REFPROP10_patch.sh $fDest)
+  fi
+done
 
 
 # ----------------------------------------
@@ -117,9 +123,15 @@ do
 done
 
 echo "Copy fluids, ppf and mixtures properties to $EOS_DATA"
-cp -rpf $PLUGINEXT_DAT/fluids   $EOS_DATA/.
-cp -rpf $PLUGINEXT_DAT/mixtures $EOS_DATA/.
+for fSrc in $PLUGINEXT_DAT/fluids/* $PLUGINEXT_DAT/mixtures/*
+do
+  fDest="$EOS_DATA/"`basename $fSrc`
+  if test ! -f $fDest -o $fSrc -nt $fDest
+  then
+    cp $fSrc $fDest
+  fi
 
+done
 
 
 # ----------------------------------------
--- Modules/EOS/Src/EOS_Refprop10/Refprop10/REFPROP10_patch.sh
+++ Modules/EOS/Src/EOS_Refprop10/Refprop10/REFPROP10_patch.sh
@@ -4,50 +4,61 @@
 #
 #
 
+fSrc=`basename $1`
 
 ##### Patch Refprop 10 source files
-echo "Patch Refprop 10 source files"
+echo "Patch Refprop 10 source file $fSrc"
 
 
+if test $fSrc == "SETUP.FOR"
+then
 # file SETUP.FOR, subroutine SETFLD :
 # change  call RFFILE (hFile(i),hreff)  to  call RFFILE (hreff,hFile(i))
-awk 'BEGIN{IGNORECASE=1; ind=0} \
+  awk 'BEGIN{IGNORECASE=1; ind=0} \
      {if ($0 ~ /^ *subroutine *SETFLD/) {ind=1 ; print} \
       else if (ind == 1 && $0 ~ /hreff.*\.ne/)  {ind=2 ; print} \
       else if (ind == 2 && $0 ~ /call *RFFILE/) {ind=0 ; sub("\\(hFile\\(i\\),hreff\\)","(hreff,hFile(i))"); sub("!","! M.F. "); print} \
       else print}'  SETUP.FOR >tmp$$ ; mv tmp$$ SETUP.FOR
 
 # file SETUP.FOR, modification  subroutine RFFILE
-:>setup.be
-:>setup.af
-awk 'BEGIN{IGNORECASE=1; ind=0} \
+  :>setup.be
+  :>setup.af
+  awk 'BEGIN{IGNORECASE=1; ind=0} \
      {if ($0 ~ /^ *subroutine *RFFILE/) {ind=1} \
       else if (ind == 1 && $0 ~ /subroutine *RFFILE/)  {ind=2} \
       else if (ind == 0) {print >>  "setup.be"} \
       else if (ind == 2) {print >>  "setup.af"}}'  SETUP.FOR
 
-cat setup.be new_rffile setup.af > SETUP.FOR
+  cat setup.be new_rffile setup.af > SETUP.FOR
 
-# file SETUP.FOR, subroutine SETMOD :
-# information -912 is now in a comment
-sed -i -e 's/^ *call *ERRNUM.*-912/* M.F. &/' SETUP.FOR
+  # file SETUP.FOR, subroutine SETMOD :
+  # information -912 is now in a comment
+  sed -i -e 's/^ *call *ERRNUM.*-912/* M.F. &/' SETUP.FOR
+fi
 
-
+if test $fSrc == "SAT_SUB.FOR"
+then 
 # file SAT_SUB.FOR, subroutine SATP : initialization T = 0.e0
-awk 'BEGIN{IGNORECASE=1; ind=0} \
+  awk 'BEGIN{IGNORECASE=1; ind=0} \
      {if ($0 ~ /^ *subroutine *SATP/) {ind=1 ; print} \
       else if (ind == 1 && $0 ~ /iSatFlg1/)  {ind=0 ; print "      T = 0.e0 ;"; print} \
       else print}'  SAT_SUB.FOR >tmp$$ ; mv tmp$$ SAT_SUB.FOR
+fi
 
+if test $fSrc == "TRNSP.FOR"
+then 
 # file TRNSP.FOR, subroutine TRNECS : initialization T0 = 0.e0
 awk 'BEGIN{IGNORECASE=1; ind=0} \
      {if ($0 ~ /^ *subroutine *TRNECS/) {ind=1 ; print} \
       else if (ind == 1 && $0 ~ /^ *call *TRNEC/)  {ind=0 ; print "      T0 = 0.e0 ;"; print} \
       else print}'  TRNSP.FOR >tmp$$ ; mv tmp$$ TRNSP.FOR
+fi
 
+fmt=`file $fSrc`
+if [[ $fmt == *"CRLF"* ]]; then
+  dos2unix $fSrc
+fi
 
-
-dos2unix *
 
 exit 0
 
